f46bd5b27166feeec9252c80700db96eed642c95
MeaninGood <jsmeaningood@gmail.com>
2023-12-03 22:12:09 +0900

    ✨ prettier 및 eslint 설정

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+export default {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 80,
+  arrowParens: 'avoid',
+  'editor.formatOnSave': true,
+  'editor.formatOnType': true,
+};
