dd022133543d999955ad16499d3d0494008aeb17
MeaninGood <jsmeaningood@gmail.com>
2023-12-04 00:22:17 +0900

    ✨ 자동배포 3
    - [작성할 부분] 내용 1
    - [작성할 부분] 내용 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
       hello! prography-homepage <br />
       자동배포 테스트입니다<br />
       <StyledTest />
-      좋은 하루 되세요
+      좋은 하루 되세요<br />
+      ㅎㅇㅎㅇ
     </div>
   );
 }
