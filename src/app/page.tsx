1ac828afdb639d1ecc968cf9b17a86fc2062942e
JangJiseon <92563854+MeaninGood@users.noreply.github.com>
2023-12-05 21:38:40 +0900

    Revert "✨ 버튼 컴포넌트 추가"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
-import StyledTest from '@/components/StyledTest';
-import Button from '@/components/common/button/Button';
+import StyledTest from "@/components/StyledTest";
 
 export default function Home() {
   return (
     <div>
       hello! prography-homepage <br />
-      자동배포 테스트입니다
-      <br />
+      자동배포 테스트입니다<br />
       <StyledTest />
-      좋은 하루 되세요
-      <br />
+      좋은 하루 되세요<br />
       ㅎㅇㅎㅇ
-      <div style={{ display: 'block', background: 'black', padding: '30px' }}>
-        <Button label="test"></Button>
-        <Button label="test" disabled></Button>
-        <Button label="test" button_size="56"></Button>
-        <Button label="test" button_size="48"></Button>
-      </div>
     </div>
   );
 }
--- src/components/StyledTest.tsx
+++ src/components/StyledTest.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import styles from './StyledTest.module.scss';
 
 const StyledTest = () => {
-  return (
-    <>
-      <button className={styles.button}>StyledTest</button>
-    </>
-  );
+  return <button className={styles.button}>StyledTest</button>;
 };
+
 
 export default StyledTest;
