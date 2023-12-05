eeefe0e886a9dcf3dabb77494cab8b826250f306
jiseonjang <jsmeaningood@gmail.com>
2023-12-05 21:13:45 +0900

    ✨ font 추가
    - global css class로 만들었습니다

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,17 @@
+import '../styles/global.css';
+import '../styles/fonts.scss';
 
 export default function RootLayout(props: React.PropsWithChildren) {
   return (
     <html>
+      <head>
+      <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin=""
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+        />
+      </head>
       <body>
           {props.children}
       </body>
