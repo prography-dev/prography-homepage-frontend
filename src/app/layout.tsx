dc95ae5f0e86efb9dbb4e7b9c81270858a4d72d4
MeaninGood <jsmeaningood@gmail.com>
2023-12-07 23:49:07 +0900

    ✨ Footer 개발
    - Footer 개발했습니다

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import '../styles/global.css';
 import '../styles/fonts.scss';
 import '../styles/colors.scss';
 
+import Footer from '@/components/Layout/Footer';
+
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="ko">
@@ -13,7 +15,10 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
