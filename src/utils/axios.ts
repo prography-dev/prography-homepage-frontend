7b5de8e46bca3e086ce69a4f3f17e002d8845500
coco-pink <coco.pink@kakaostyle.com>
2023-12-09 23:55:40 +0900

    📦 axios 모듈 추가
    - axios 모듈 추가

---
+++ src/utils/axios.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export const BASE_URL = '';
+// export const BASE_URL = 'http://localhost:3000/api'
+
+export const instance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
+
+export function get<T>(...args: Parameters<typeof instance.get>) {
+  return instance.get<T, T>(...args);
+}
+
+export function post<T>(...args: Parameters<typeof instance.post>) {
+  return instance.post<T, T>(...args);
+}
+
+export function put<T>(...args: Parameters<typeof instance.put>) {
+  return instance.put<T, T>(...args);
+}
+
+export function patch<T>(...args: Parameters<typeof instance.patch>) {
+  return instance.patch<T, T>(...args);
+}
+
+export function del<T>(...args: Parameters<typeof instance.delete>) {
+  return instance.delete<T, T>(...args);
+}
