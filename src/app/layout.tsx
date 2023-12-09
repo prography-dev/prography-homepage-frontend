3bae23d32fdfbbfadf039814872e7482879eb2b6
coco-pink <coco.pink@kakaostyle.com>
2023-12-09 13:59:01 +0900

    🔨Footer 컴포넌트 디렉토리 구조 변경
    - Footer 컴포넌트 디렉토리 구조 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import '../styles/global.css';
 import '../styles/fonts.scss';
 import '../styles/colors.scss';
 
-import Footer from '@/components/Layout/Footer';
+import Footer from '@/components/Layout/footer/Footer';
 import Header from '@/components/Layout/header/Header';
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
