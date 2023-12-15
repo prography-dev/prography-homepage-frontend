87fe2895f75bb2efd568020843756d7189f91daf
MeaninGood <jsmeaningood@gmail.com>
2023-12-16 02:44:26 +0900

    ✨ 모달 Contents부분 개발

--- src/components/Modal/ModalContents.tsx
+++ src/components/Modal/ModalContents.tsx
@@ -1,7 +1,20 @@
-import './ModalContents.module.scss';
+import Image from 'next/image';
+import styles from './ModalContents.module.scss';
 
 const ModalContents = () => {
-  return <div className="contents-wrapper">ModalContents</div>;
+  return (
+    <div className={`${styles.ContentsWrapper}`}>
+      <div className="pre_body_2">ModalContents</div>
+      <Image
+        src="
+      https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjMy/MDAxNjQzMTAyOTg1NzI0.bkW6TJVG82Gi8uG643n5SaSTYOyEcNAq0Y7xsEkOBSUg.rU7SY3uYHJGnigm3WzvBk0LkXt_cO6UOyVsfeKxbEPAg.JPEG.minziminzi128/IMG_7370.JPG?type=w800"
+        alt="thumbnail"
+        width={100}
+        height={100}
+        layout="responsive"
+      />
+    </div>
+  );
 };
 
 export default ModalContents;
