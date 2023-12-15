3fa9aa60af3d106de27e386cbbc62042cf54c597
MeaninGood <jsmeaningood@gmail.com>
2023-12-15 23:46:29 +0900

    🔨 Behance Icon에 props 추가

--- src/components/common/icon/IconBehance.tsx
+++ src/components/common/icon/IconBehance.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-const IconBehance = (): JSX.Element => {
+import { IconProps } from '.';
+
+const IconBehance = ({ size = 50 }: IconProps): JSX.Element => {
   return (
     <svg
-      width="50"
-      height="50"
+      width={size}
+      height={size}
       viewBox="0 0 50 50"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
