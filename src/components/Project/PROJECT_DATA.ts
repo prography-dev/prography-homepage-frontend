40981e3e34cb09e74b1dd00b4ed41a74bf5f0667
MeaninGood <jsmeaningood@gmail.com>
2023-12-17 23:12:39 +0900

    🔨 더미데이터 추가

--- src/components/Project/PROJECT_DATA.ts
+++ src/components/Project/PROJECT_DATA.ts
@@ -461,4 +461,151 @@ export const PROJECT_DATA: ProjectCardData[] = [
       },
     ],
   },
+  {
+    id: 9,
+    title: '둥둥',
+    description: '나갈까',
+    generation: 7,
+    thumbnailUrl:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjg0/MDAxNjQzMTAyOTg0Nzgz.sLrqOJ2S3r6pLboUm5yJTjB_JECC0zO9Tt3y_h86aJcg.w5VER_KDRAW3yRq8-nypsm2aGmKurM5YieSFcr1Vg0Qg.JPEG.minziminzi128/IMG_7374.JPG?type=w800',
+    imageUrl:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjMy/MDAxNjQzMTAyOTg1NzI0.bkW6TJVG82Gi8uG643n5SaSTYOyEcNAq0Y7xsEkOBSUg.rU7SY3uYHJGnigm3WzvBk0LkXt_cO6UOyVsfeKxbEPAg.JPEG.minziminzi128/IMG_7370.JPG?type=w800',
+    stacks: ['Java', 'React', 'Spring Boot', 'MySQL', 'AWS EC2', 'Type Script'],
+    users: [
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 222,
+        name: '또민성',
+        part: 'Android',
+      },
+    ],
+    socials: [
+      {
+        type: 'INSTAGRAM',
+        landingUrl: 'https://www.instagram.com/prography_official/',
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: '둥둥',
+    description: '나갈까',
+    generation: 7,
+    thumbnailUrl:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjg0/MDAxNjQzMTAyOTg0Nzgz.sLrqOJ2S3r6pLboUm5yJTjB_JECC0zO9Tt3y_h86aJcg.w5VER_KDRAW3yRq8-nypsm2aGmKurM5YieSFcr1Vg0Qg.JPEG.minziminzi128/IMG_7374.JPG?type=w800',
+    imageUrl:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjMy/MDAxNjQzMTAyOTg1NzI0.bkW6TJVG82Gi8uG643n5SaSTYOyEcNAq0Y7xsEkOBSUg.rU7SY3uYHJGnigm3WzvBk0LkXt_cO6UOyVsfeKxbEPAg.JPEG.minziminzi128/IMG_7370.JPG?type=w800',
+    stacks: ['Java', 'React', 'Spring Boot', 'MySQL', 'AWS EC2', 'Type Script'],
+    users: [
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 222,
+        name: '또민성',
+        part: 'Android',
+      },
+    ],
+    socials: [
+      {
+        type: 'INSTAGRAM',
+        landingUrl: 'https://www.instagram.com/prography_official/',
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: '둥둥',
+    description: '나갈까',
+    generation: 7,
+    thumbnailUrl:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjg0/MDAxNjQzMTAyOTg0Nzgz.sLrqOJ2S3r6pLboUm5yJTjB_JECC0zO9Tt3y_h86aJcg.w5VER_KDRAW3yRq8-nypsm2aGmKurM5YieSFcr1Vg0Qg.JPEG.minziminzi128/IMG_7374.JPG?type=w800',
+    imageUrl:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjMy/MDAxNjQzMTAyOTg1NzI0.bkW6TJVG82Gi8uG643n5SaSTYOyEcNAq0Y7xsEkOBSUg.rU7SY3uYHJGnigm3WzvBk0LkXt_cO6UOyVsfeKxbEPAg.JPEG.minziminzi128/IMG_7370.JPG?type=w800',
+    stacks: ['Java', 'React', 'Spring Boot', 'MySQL', 'AWS EC2', 'Type Script'],
+    users: [
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 1,
+        name: '또범우',
+        part: 'Designer',
+      },
+      {
+        id: 222,
+        name: '또민성',
+        part: 'Android',
+      },
+    ],
+    socials: [
+      {
+        type: 'INSTAGRAM',
+        landingUrl: 'https://www.instagram.com/prography_official/',
+      },
+    ],
+  },
 ];
