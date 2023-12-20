c9c8256175852a48e0b5207219e1c81bfd885e9c
jiseonjang <jsmeaningood@gmail.com>
2023-12-20 14:29:20 +0900

    ✨모바일 아닐 때 페이지네이션 추가

--- src/components/Modal/OtherProjects.tsx
+++ src/components/Modal/OtherProjects.tsx
@@ -5,11 +5,11 @@ import ProjectCardContainer from '../Project/ProjectCardContainer';
 import styles from './OtherProjects.module.scss';
 import { ProjectCardData } from '@/apis/project';
 import Icon80RoundButton from '../common/icon/Icon80RoundButton';
-import { isPcDevice, isTabletDevice } from '@/utils/device';
+import { isPhoneDevice } from '@/utils/device';
+import Icon64PageButton from '../common/icon/Icon64PageButton';
 
 const itemsPerPage = {
   pc: 3,
-  tablet: 2,
   mobile: 4,
 };
 
@@ -26,12 +26,35 @@ const OtherProjects = ({
   onClick,
   otherProjects,
 }: OtherProjectsProps) => {
+  const isMobile = isPhoneDevice();
+
   const [currentPage, setCurrentPage] = useState(1);
   const [projectData, setProjectData] =
     useState<ProjectCardData[]>(otherProjects);
 
-  const handlePageChange = () => {
+  let pagenationCount: number;
+
+  if (isMobile) {
+    pagenationCount = itemsPerPage.mobile;
+  } else {
+    pagenationCount = itemsPerPage.pc;
+  }
+
+  const isAllProjects = pagenationCount * currentPage >= 11;
+
+  const lastPageNumber = Math.ceil(otherProjects.length / pagenationCount) ?? 0;
+  const totalPage = Array.from({ length: lastPageNumber }, (_, i) => i + 1);
+
+  const handlePageCountUp = () => {
     setCurrentPage(currentPage + 1);
+  };
+
+  const handlePageCountDown = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleNumberPageChange = (target: number) => {
+    setCurrentPage(target);
   };
 
   // 현재 열려있는 프로젝트 제외, Other Projects 배열
@@ -50,33 +73,72 @@ const OtherProjects = ({
     onSelectOtherPjt(target);
   };
 
-  let pagenationCount: number;
-  if (isPcDevice()) {
-    pagenationCount = itemsPerPage.pc;
-  } else if (isTabletDevice()) {
-    pagenationCount = itemsPerPage.tablet;
-  } else {
-    pagenationCount = itemsPerPage.mobile;
-  }
-
-  const isAllProjects = pagenationCount * currentPage >= 11;
-
   return (
     <div className={styles.OtherPjtWrapper}>
       <div className="sf_h3_to_h1">Other projects</div>
-      <ProjectCardContainer
-        projects={projectData.slice(0, pagenationCount * currentPage)}
-        onChange={onSelectCard}
-        onClick={onClick}
-      />
-      <div
-        className={`${styles.ArrowIconDiv} ${
-          isAllProjects ? styles.Hidden : ''
-        }`}
-        onClick={handlePageChange}
-      >
-        <Icon80RoundButton />
-      </div>
+
+      {isMobile ? (
+        <>
+          <ProjectCardContainer
+            projects={projectData.slice(0, pagenationCount * currentPage)}
+            onChange={onSelectCard}
+            onClick={onClick}
+          />
+
+          <div
+            className={`${styles.ArrowIconDiv} ${
+              isAllProjects ? styles.Hidden : ''
+            }`}
+            onClick={handlePageCountUp}
+          >
+            <Icon80RoundButton />
+          </div>
+        </>
+      ) : (
+        <>
+          <ProjectCardContainer
+            projects={projectData.slice(
+              (currentPage - 1) * pagenationCount,
+              currentPage * pagenationCount,
+            )}
+            onChange={onSelectCard}
+            onClick={onClick}
+          />
+          <div className={styles.PagenationDiv}>
+            <div
+              className={`${
+                currentPage === 1
+                  ? styles.NumberButtonDisabled
+                  : styles.NumberButtonDiv
+              }`}
+              onClick={handlePageCountDown}
+            >
+              <Icon64PageButton />
+            </div>
+            {totalPage.map(el => (
+              <div
+                key={el}
+                className={`${styles.PageNumber} ${
+                  el === currentPage ? styles.PageNumberActive : ''
+                } gray600 pre_body_2`}
+                onClick={() => handleNumberPageChange(el)}
+              >
+                {el}
+              </div>
+            ))}
+            <div
+              className={`${
+                currentPage === lastPageNumber
+                  ? styles.NumberButtonDisabled
+                  : styles.NumberButtonDiv
+              }`}
+              onClick={handlePageCountUp}
+            >
+              <Icon64PageButton rotate={180} />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
