3804dc5400aea3eec38dc0a4b1c5e557abc56be3
Songminseon <smsun0329@naver.com>
2023-12-23 00:42:16 +0900

    ✨ About 통계값 api 호출

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -1,23 +1,23 @@
 import { useEffect, useState } from 'react';
 import CountUp from 'react-countup';
-import styles from './About.module.scss';
 
-interface CountProps {
-  applicant: number;
-  project: number;
-}
+import styles from './About.module.scss';
+import { StatisticType, getStatistics } from '@/apis/statistics';
 
 const About = () => {
   const [loading, setLoading] = useState(false);
-  const [count, setCount] = useState<CountProps>({
-    applicant: 0,
-    project: 0,
+  const [count, setCount] = useState<StatisticType>({
+    avgApplicant: 0,
+    projectNumber: 0,
   });
 
   useEffect(() => {
-    // toDo: api 교체 필요
-    setCount({ ...count, applicant: 100, project: 50 });
-    setLoading(true);
+    const fetchData = async () => {
+      const data = await getStatistics();
+      setCount({ ...count, ...data });
+      setLoading(true);
+    };
+    fetchData();
   }, []);
 
   const defaultOptions = {
@@ -30,7 +30,7 @@ const About = () => {
     <div className={styles.PgCountupContainer}>
       <div className={styles.PgCountupWrapper}>
         <CountUp
-          end={count.applicant}
+          end={count.avgApplicant}
           aria-busy={loading}
           {...defaultOptions}
         />
@@ -38,7 +38,11 @@ const About = () => {
         <p className={styles.PgCountupLabel}>평균 지원자</p>
       </div>
       <div className={styles.PgCountupWrapper}>
-        <CountUp end={count.project} aria-busy={loading} {...defaultOptions} />
+        <CountUp
+          end={count.projectNumber}
+          aria-busy={loading}
+          {...defaultOptions}
+        />
         <span className={styles.PgCountupValue}>+</span>
         <p className={styles.PgCountupLabel}>프로젝트</p>
       </div>
