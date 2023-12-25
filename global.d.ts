8991e95616258079ca41e4ae22d7cefc3702427b
Songminseon <smsun0329@naver.com>
2023-12-25 17:27:34 +0900

    ✨ gtm 기능 추가

---
+++ global.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable*/
+
+interface Window {
+  dataLayer: any[]; // 또는 실제로 예상되는 dataLayer의 타입을 지정하세요.
+}
--- src/components/Home/LandingButton.tsx
+++ src/components/Home/LandingButton.tsx
@@ -2,11 +2,18 @@
 
 import styles from './LandingButton.module.scss';
 import Button from '@/components/common/button/Button';
+import triggerGtm from '@/utils/triggerGtm';
 
 const LandingButton = () => {
   const handleApplyButton = () => {
     // toDo: 구글 폼 나온 후 수정 필요
     window.open('https://prography.org/');
+    triggerGtm({
+      event: '지원버튼',
+      parameter: {
+        path: '/',
+      },
+    });
   };
   return (
     <div className={styles.PgApplyButtonContainer}>
