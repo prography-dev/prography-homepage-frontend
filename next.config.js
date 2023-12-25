629bdb90734d8678b01608a3dba7a959e236dc9d
MeaninGood <jsmeaningood@gmail.com>
2023-12-25 18:29:46 +0900

    🔨 next.config.js 내 domain 수정
    - 고양이 이미지 도메인 지웠던 거 복구

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@
  */
 const nextConfig = {
   images: {
-    domains: ['s3.ap-northeast-2.amazonaws.com'],
+    domains: [
+      'mblogthumb-phinf.pstatic.net',
+      's3.ap-northeast-2.amazonaws.com',
+    ],
   },
   reactStrictMode: false,
   async rewrites() {
