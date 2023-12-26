c878388c56789238cff2230cfc40d15b4d6440b1
Songminseon <smsun0329@naver.com>
2023-12-27 00:50:02 +0900

    ✨ header generation api 호출 및 연동

--- src/components/Layout/header/Header.tsx
+++ src/components/Layout/header/Header.tsx
@@ -1,74 +1,15 @@
-'use client';
+import HeaderComponent from './HeaderComponent';
+import { getLastGeneration, GenerationType } from '@/apis/generation';
 
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import Button from '@/components/common/button/Button';
-import styles from './Header.module.scss';
-import { IconClose, IconMenu } from '@/components/common/icon';
+async function getGenerationData() {
+  return getLastGeneration();
+}
 
-const Header = () => {
-  const router = useRouter();
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+const Header = async () => {
+  const generation = await getGenerationData();
+  const { name, status, landingUrl } = generation as GenerationType;
 
-  const onClickCategory = (pathname: string) => {
-    setIsMobileMenuOpen(false);
-    router.push(pathname);
-  };
-
-  return (
-    <header
-      className={`${styles.HeaderContainer} ${
-        isMobileMenuOpen ? styles.MobileMenuOpen : styles.MobileMenuClose
-      }`}
-    >
-      <div
-        className={
-          isMobileMenuOpen ? styles.MobileMenuContainer : styles.Content
-        }
-      >
-        <Link href="/" className={styles.HeaderLogo}>
-          <img src="/assets/logos/logo_prography.svg" alt="logo" />
-        </Link>
-
-        {isMobileMenuOpen ? (
-          <IconClose
-            color="white"
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-          />
-        ) : (
-          <IconMenu
-            color="white"
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-          />
-        )}
-
-        <ul>
-          <li
-            className={isMobileMenuOpen ? 'sf_heading_3' : 'sf_caption_1'}
-            onClick={() => onClickCategory('/about')}
-          >
-            About
-          </li>
-          <li
-            className={isMobileMenuOpen ? 'sf_heading_3' : 'sf_caption_1'}
-            onClick={() => onClickCategory('/project')}
-          >
-            Project
-          </li>
-          <li
-            className={isMobileMenuOpen ? 'sf_heading_3' : 'sf_caption_1'}
-            onClick={() => onClickCategory('/contact')}
-          >
-            Contact
-          </li>
-          <li>
-            <Button buttonSize="56">9기 지원</Button>
-          </li>
-        </ul>
-      </div>
-    </header>
-  );
+  return <HeaderComponent name={name} status={status} url={landingUrl} />;
 };
 
 export default Header;
