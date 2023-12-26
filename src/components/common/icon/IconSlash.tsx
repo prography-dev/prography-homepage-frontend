532da3e1016b9b7d0adf1d7faec780941d4a5548
MeaninGood <jsmeaningood@gmail.com>
2023-12-27 00:55:03 +0900

    🔨 Modal Crew 내 QA 반영
    - 최소 gap: 14px로 수정
    - Crew name / part 사이 slash svg로 교체

---
+++ src/components/common/icon/IconSlash.tsx
@@ -0,0 +1,17 @@
+import '@/styles/const.scss';
+
+const IconSlash = () => {
+  return (
+    <svg
+      width="6"
+      height="14"
+      viewBox="0 0 6 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M5 1L1 13" stroke="#47474B" />
+    </svg>
+  );
+};
+
+export default IconSlash;
