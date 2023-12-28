17e5d300ab311a1e973475c6940125485afe9db7
MeaninGood <jsmeaningood@gmail.com>
2023-12-29 01:52:10 +0900

    ✨ socials type에 따라 Icon 컴포넌트 렌더링하는 공통함수 개발, 타입 수정
    - 타입 수정하고, 공통 함수 개발해서 Icon에 반영했습니다
    - useState에 쓸 초기값 타입용 EMPTY 타입 추가했습니다.

--- src/apis/project.ts
+++ src/apis/project.ts
@@ -1,5 +1,17 @@
 import { getData } from '.';
 
+export interface SocialsType {
+  type:
+    | 'BEHANCE'
+    | 'GITHUB_BE'
+    | 'GITHUB_FE'
+    | 'INSTAGRAM'
+    | 'APP_STORE'
+    | 'GOOGLE_PLAY'
+    | 'WEB'
+    | 'EMPTY';
+  landingUrl: string;
+}
 export interface ProjectType {
   id: number;
   title: string;
@@ -13,10 +25,7 @@ export interface ProjectType {
     name: string;
     part: string;
   }[];
-  socials: {
-    type: string;
-    landingUrl: string;
-  }[];
+  socials: SocialsType[];
 }
 
 export const getProjectData = () => getData<ProjectType[]>('projects');
