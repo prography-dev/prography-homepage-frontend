c353db95faad6a13184739d156cad673c0f6e2fe
Songminseon <smsun0329@naver.com>
2023-12-29 01:43:22 +0900

    ✨ gtm client용 스크립트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,15 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="ko">
       <head>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5898M2RG');
+        `}
+        </Script>
         <link
           rel="stylesheet"
           as="style"
@@ -58,6 +67,16 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
         <Header />
         {children}
         <Footer />
+
+        <noscript>
+          <iframe
+            title="gtm"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5898M2RG"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
       </body>
     </html>
   );
